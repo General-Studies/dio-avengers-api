9b4c995c5a3f1d6784fea49197e896febcca81d6
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-04-01 22:23:47 -0300

    created migration

---
+++ src/main/resources/db/migration/V20210401222100__create_avenger_table.sql
@@ -0,0 +1,10 @@
+create table avenger (
+    id bigserial not null,
+    nick varchar(36),
+    person varchar(128),
+    description varchar(128),
+    history text
+    primary key (id)
+);
+
+alter table avenger add constraint UK_5r88eemotwgru6k0ilqb2ledh unique (nick);
