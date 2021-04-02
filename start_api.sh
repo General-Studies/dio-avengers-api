efe0be4797946f51a78f99391d4457443f06b469
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-04-01 22:19:34 -0300

    created start api shell script

---
+++ start_api.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=dio.avenger' '--DB_PASSWORD=dio.avenger' '--DB_NAME=avengers'"
