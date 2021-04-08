f03b695fb4ba254cbd849aaae67179108ce824ae
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-04-08 20:34:19 -0300

    updated to remove commas

--- src/main/kotlin/one/digitalinnovation/avengers/application/web/resource/request/AvengerRequest.kt
+++ src/main/kotlin/one/digitalinnovation/avengers/application/web/resource/request/AvengerRequest.kt
@@ -14,8 +14,8 @@ data class AvengerRequest(
     @field:NotBlank
     @field:NotEmpty
     val person: String,
-    val description: String? = "",
-    val history: String? = ""
+    val description: String? = null,
+    val history: String? = null
 ) {
     fun toAvenger() = Avenger(
         nick = nick,
